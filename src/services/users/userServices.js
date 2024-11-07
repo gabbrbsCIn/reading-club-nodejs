16f8e5d3bc7c19407046b2662f78162b64a4588c
gbgo <gbgo@cin.ufpe.br>
2024-11-07 19:33:13 -0300

    feat: add funções findUserByEmail, userAuthenticate, generateJWTToken ao arquivo de services

--- src/services/users/userServices.js
+++ src/services/users/userServices.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
 const prisma = require("../../../prisma/prismaClient");
-const { generateHashPassword } = require("../../utils/validators/validators");
+const { NotFoundError } = require("../../errors/errors");
+const {
+  generateHashPassword,
+  verifyPassword,
+} = require("../../utils/validators/validators");
+
+const jwt = require("jsonwebtoken");
 
 const createUser = async (data) => {
   const hashedPassword = await generateHashPassword(data.password);
@@ -12,4 +19,39 @@ const createUser = async (data) => {
   });
   return user.email;
 };
-module.exports = { createUser };
+
+const findUserByEmail = async (email) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  return user;
+};
+
+const userAuthenticate = async (data) => {
+  const user = await findUserByEmail(data.email);
+  if (!user) {
+    throw new NotFoundError("E-mail ou senha inválidos");
+  }
+  await verifyPassword(data.password, user.password);
+  return user;
+};
+
+const generateJWTToken = (user) => {
+  const payload = { id: user.id };
+  const secretKey = process.env.JWT_SECRET_KEY;
+  console.log(process.env.JWT_EXPIRES_IN);
+  const options = {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  };
+
+  const token = jwt.sign(payload, secretKey, options);
+  return token;
+};
+module.exports = {
+  createUser,
+  userAuthenticate,
+  findUserByEmail,
+  generateJWTToken,
+};
