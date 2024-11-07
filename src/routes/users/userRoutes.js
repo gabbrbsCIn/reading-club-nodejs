a1a631bc265aa4b89977f19a18986c46fe039f95
gbgo <gbgo@cin.ufpe.br>
2024-11-07 19:32:40 -0300

    feat: add rota para chamar controller login

--- src/routes/users/userRoutes.js
+++ src/routes/users/userRoutes.js
@@ -4,5 +4,6 @@ const router = Router();
 const userController = require("../../controllers/users/userControllers");
 
 router.post("/user", userController.register);
+router.post("/login", userController.login);
 
 module.exports = router;
