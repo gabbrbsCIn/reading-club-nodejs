4b02bb3c4c1922ffd51debf97144707d667101a0
gbgo <gbgo@cin.ufpe.br>
2024-11-08 10:13:03 -0300

    feat: add rota para chamar controller joinClub

--- src/routes/clubs/clubRoutes.js
+++ src/routes/clubs/clubRoutes.js
@@ -4,5 +4,6 @@ const clubControllers = require("../../controllers/clubs/clubControllers");
 const authToken = require("../../middlewares/authMiddleware");
 
 router.post("/club", authToken, clubControllers.register);
+router.post("/club/join/:id", authToken, clubControllers.joinClub);
 
 module.exports = router;
