8f38910b279c355c0f656837c3d0478c49bf8c9f
gbgo <gbgo@cin.ufpe.br>
2024-11-08 09:31:31 -0300

    feat: add rota para chamar controller destroy

--- src/routes/users/userRoutes.js
+++ src/routes/users/userRoutes.js
@@ -5,6 +5,7 @@ const userController = require("../../controllers/users/userControllers");
 const authToken = require("../../middlewares/authMiddleware");
 router.post("/user", userController.register);
 router.put("/user", authToken, userController.update);
+router.delete("/user", authToken, userController.destroy);
 router.post("/login", userController.login);
 
 module.exports = router;
