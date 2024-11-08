336a9a4ff978c2d728f3cea89091d1131518bc98
gbgo <gbgo@cin.ufpe.br>
2024-11-08 10:58:59 -0300

    feat: add rota para chamar controller update

--- src/routes/clubs/clubRoutes.js
+++ src/routes/clubs/clubRoutes.js
@@ -2,8 +2,10 @@ const { Router } = require("express");
 const router = Router();
 const clubControllers = require("../../controllers/clubs/clubControllers");
 const authToken = require("../../middlewares/authMiddleware");
+const { isClubJoined } = require("../../middlewares/clubMiddleware");
 
 router.post("/club", authToken, clubControllers.register);
 router.post("/club/join/:id", authToken, clubControllers.joinClub);
+router.put("/club/:id", authToken, isClubJoined, clubControllers.update);
 
 module.exports = router;
