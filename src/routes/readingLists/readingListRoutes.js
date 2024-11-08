de158103e9c05cfedc508e7336cd685482c56299
gbgo <gbgo@cin.ufpe.br>
2024-11-08 17:35:19 -0300

    feat: add controller para chamar controller update

--- src/routes/readingLists/readingListRoutes.js
+++ src/routes/readingLists/readingListRoutes.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
 const authToken = require("../../middlewares/authMiddleware");
-const { isClubJoined } = require("../../middlewares/clubMiddleware");
+const {
+  isClubJoined,
+  isListJoined,
+} = require("../../middlewares/clubMiddleware");
 const readingListController = require("../../controllers/readingLists/readingListControllers");
 
 const router = Router();
 
-router.post("/list/:id", authToken, isClubJoined, readingListController.register);
+router.post(
+  "/list/:id",
+  authToken,
+  isClubJoined,
+  readingListController.register
+);
+router.put("/list/:id", authToken, isListJoined, readingListController.update);
 
 module.exports = router;
