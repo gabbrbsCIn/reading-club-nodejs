f93a4a76a54d0617f292a7179e547e0a1ac6be14
gbgo <gbgo@cin.ufpe.br>
2024-11-08 17:34:05 -0300

    feat: add funções findClubIdByListId e updateReadingList ao arquivo de services

--- src/services/readingLists/readingListServices.js
+++ src/services/readingLists/readingListServices.js
@@ -1,4 +1,5 @@
 const prisma = require("../../../prisma/prismaClient");
+const { AuthorizationError } = require("../../errors/errors");
 
 const createReadingList = async (list, clubId) => {
   const readingList = await prisma.readingList.create({
@@ -9,4 +10,33 @@ const createReadingList = async (list, clubId) => {
   });
   return readingList;
 };
-module.exports = { createReadingList };
+
+const findClubIdByListId = async (listId) => {
+  const club = await prisma.readingList.findUnique({
+    where: {
+      id: listId,
+    },
+    select: {
+      clubId: true,
+    },
+  });
+  if (!club) {
+    throw new AuthorizationError(
+      "Você não tem permissão para alterar as listas desse clube"
+    );
+  }
+  return club.clubId;
+};
+
+const updateReadingList = async (list, listId) => {
+  const readingList = await prisma.readingList.update({
+    where: {
+      id: listId,
+    },
+    data: {
+      name: list.name,
+    },
+  });
+  return readingList;
+};
+module.exports = { createReadingList, findClubIdByListId, updateReadingList };
