40ab11df12e352e3bbf168f0747b3fc678ea8cdb
gbgo <gbgo@cin.ufpe.br>
2024-11-08 18:44:57 -0300

    feat: add funções findBookById e addBookToList ao arquivo de services

--- src/services/books/bookServices.js
+++ src/services/books/bookServices.js
@@ -1,8 +1,33 @@
 const prisma = require("../../../prisma/prismaClient");
+const { NotFoundError } = require("../../errors/errors");
 
 const createBook = async (bookData) => {
   const book = await prisma.book.create({ data: bookData });
   return book;
 };
 
-module.exports = { createBook };
+const findBookById = async (bookId) => {
+  const book = await prisma.book.findFirst({
+    where: {
+      id: bookId,
+    },
+  });
+  if (!book) {
+    throw new NotFoundError("Livro não encontrado");
+  }
+  return book;
+};
+
+const addBookToList = async (listId, bookId) => {
+  const bookList = await prisma.readingListBook.create({
+    data: {
+      readingListId: listId,
+      bookId: bookId,
+      status: "to-read"
+    },
+  });
+
+  return bookList;
+};
+
+module.exports = { createBook, findBookById, addBookToList };
