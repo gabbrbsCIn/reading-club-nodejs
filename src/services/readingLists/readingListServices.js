c61ed0ad573d89a65ec486ab9d6e3e7b815b2508
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-11-09 20:43:23 -0300

    feat: add funções updateReadingStatus e findReadingListById ao arquivo de services

--- src/services/readingLists/readingListServices.js
+++ src/services/readingLists/readingListServices.js
@@ -1,5 +1,5 @@
 const prisma = require("../../../prisma/prismaClient");
-const { AuthorizationError } = require("../../errors/errors");
+const { AuthorizationError, NotFoundError } = require("../../errors/errors");
 
 const createReadingList = async (list, clubId) => {
   const readingList = await prisma.readingList.create({
@@ -52,9 +52,49 @@ const deleteReadingList = async (listId) => {
 
   return readingList;
 };
+
+const findReadingListById = async (listId, bookId) => {
+  const bookList = await prisma.readingListBook.findUnique({
+    where: {
+      readingListId_bookId: {
+        readingListId: listId,
+        bookId: bookId,
+      },
+    },
+  })
+
+  if (!bookList) {
+    throw new NotFoundError("Livro não encontrado na lista");
+  }
+
+  return bookList;
+
+};
+
+const updateReadingStatus = async (listId, bookId, status) => {
+  const readingListBook = await prisma.readingListBook.update({
+    where: {
+      readingListId_bookId: {
+        bookId: bookId,
+        readingListId: listId,
+      },
+    },
+    data: {
+      status: status,
+    },
+  })
+
+  if (!readingListBook) {
+    throw new NotFoundError("Livro não encontrado na lista");
+  }
+
+  return readingListBook;
+}
 module.exports = {
   createReadingList,
   findClubIdByListId,
   updateReadingList,
   deleteReadingList,
+  updateReadingStatus,
+  findReadingListById,
 };
